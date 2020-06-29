fd226ad72b584a245712e4cc3963a76ba401961e
WhitneyChase <64234289+WhitneyChase@users.noreply.github.com>
2020-06-29 17:43:02 -0600

    Update notes.js

--- index/week-4/notes.js
+++ index/week-4/notes.js
@@ -1,1 +1,9 @@
 
+Javascript and object-oriented programming, 
+Works very similarly to other languages, and the main parts to focus on are encapsulation and inheritance. You can use the ubiquitous object literal pattern                                                                                                             var object = { variable: “stuff”, otherVariable: “stuff”}; 
+Or you can have a more dimensional object using the prototype method: 
+Function blah blah () {} 
+Function.object.thingInObjectYouWant = “blah blah blah”; 
+-you can add a whole bunch of stuff in here which is cool.  
+You can make classes which are labeled functions in javascript but they act likes classes. By using classes you can then have the all important inheritance properties. Encapsulation put simply is using a class(or in this case function) constructor. 
+The Object.create method is really interesting, seeing how it inherits from the object that you pass into it.
