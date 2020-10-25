69bf687adbc9773f2a3f3422ff2e18ba38513250
WhitneyChase <64234289+WhitneyChase@users.noreply.github.com>
2020-10-24 23:48:11 -0600

    Update todo.js

--- index/todo/todo.js
+++ index/todo/todo.js
@@ -2,22 +2,31 @@ import utils from './utils.js';
 import ls from './ls.js';
 
 document.querySelector('#addBtn').onclick = newTodo; 
+const todoList = ls.getTodoList();
+
+let i = (todoList.length -1);
 
 function loadTodos(){
     const todoList = ls.getTodoList(); 
      todoList.forEach(todo => {
-         const el = createTodoElement(todo)
-         addToList(el);
+         const el = createTodoElement(todo); 
+         todoList.push(el);
+        //  addToList(el);
      })
 }
+
+//displays the todos stored in the todo list stored in the ls array, local storage
+loadTodos();
 
 
 function newTodo (){
     const todo = createTodo(); 
     const todoDiv = createTodoElement(todo); 
     addToList(todoDiv); 
+    todoList.push(todo);
     ls.saveTodo(todo);
 }
+
 
 function createTodo(){
     const input = document.querySelector('#todoInput'); 
@@ -26,17 +35,22 @@ function createTodo(){
     return newTodo;
 }
 
+
 function createTodoElement(todo){
     ///todo div 
     const todoDiv = document.createElement('div'); 
     todoDiv.classList.add('todo'); 
 
+
     //complete btn
-     const completeBtn = document.createElement('button'); 
-     completeBtn.setAttribute('data-completed', todo.completed, 'data-content', todo.content);  
+    const completeBtn = document.createElement('INPUT'); 
+    completeBtn.setAttribute('type', "checkbox");
+    completeBtn.setAttribute('data-completed', todo.completed, 'data-content', todo.content);  
     completeBtn.classList.add('complete-btn'); 
+    completeBtn.setAttribute('data-id', todo.id); 
     completeBtn.innerText = "Complete"; 
     completeBtn.onclick = crossedOut;
+
 
     //todo content 
     const todoContent = document.createElement('div'); 
@@ -53,35 +67,53 @@ function createTodoElement(todo){
     deleteBtn.onclick = deleteTodo;
     
 
-
     todoDiv.appendChild(completeBtn); 
     todoDiv.appendChild(todoContent); 
     todoDiv.appendChild(deleteBtn); 
 
     return todoDiv;
-
 }
+
 
 function addToList(todoDiv){
     // add to the document 
-    document.querySelector('#todos').appendChild(todoDiv); 
-
+    document.querySelector('#todos').appendChild(todoDiv);
 }
-
-
 
 
 // Event Handlers 
 function deleteTodo(e){
     const btn = e.currentTarget; 
     ls.deleteTodo(btn.getAttribute('data-id'));
-    document.querySelector('#todos').innerHTML = ''; 
-     loadTodos();
+    // document.querySelector('#todos').innerHTML = ''; 
+    // loadTodos();
 }
+
 
 function crossedOut(e){
     const btn = e.currentTarget; 
-    ls.crossedOut(btn.getAttribute('data-completed', 'data-content'));
-    // /document.querySelector('#todos').innerHTML = ''; 
-    //  loadTodos();
+    btn.completed = true; 
+    for(let i = 0; i < todoList.length; i++){
+        if (btn.id === todoList[i]){
+            btn.content = todoList[i].content;
+
+            if (btn.completed === true){
+                btn.content.line = "line-through"; 
+                document.getElementById("todo-content").style.textDecoration = todoList[i].line;
+            }
+        }
+
+
+        
 }
+    
+    console.log("completed: " + btn.completed); 
+    ls.crossedOut(btn.getAttribute('data-completed', 'data-content'));
+    // document.querySelector('#todos').innerHTML = ''; 
+    // loadTodos();
+}
+
+
+
+
+
